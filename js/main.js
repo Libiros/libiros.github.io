9deaa900473d89fd7d2d9267c6c192366bf317eb
Michael Fedotov <libiros@gmail.com>
2020-04-16 13:47:06 +0300

    Убрал часть лишнего CSS. Улучшил внешний вид. Оптимизировал JS. Убрал выбор языков. Добавил иконки.

--- js/main.js
+++ js/main.js
@@ -3,19 +3,30 @@ window.addEventListener("DOMContentLoaded",function() {
   const DARK_MODE = document.getElementById("dark_mode");
 
   LIGHT_MODE.addEventListener("click", () => {
-    document.body.classList.add("light-mode");
-    document.body.classList.remove("dark-mode");
+    lightModeOn();
   });
   DARK_MODE.addEventListener("click", () => {
-    document.body.classList.add("dark-mode");
-    document.body.classList.remove("light-mode");
+    darkModeOn();
   })
 
   if (window.matchMedia('(prefers-color-scheme: dark)').media !== 'not all') {
-    document.body.classList.add("dark-mode");
-    document.body.classList.remove("light-mode");
+    darkModeOn();
   } else {
+    lightModeOn();
+  }
+
+  function lightModeOn() {
     document.body.classList.add("light-mode");
     document.body.classList.remove("dark-mode");
+    LIGHT_MODE.classList.add("active");
+    DARK_MODE.classList.remove("active");
   }
+  
+  function darkModeOn() {
+    document.body.classList.add("dark-mode");
+    document.body.classList.remove("light-mode");
+    LIGHT_MODE.classList.remove("active");
+    DARK_MODE.classList.add("active");
+  }
+
 })
